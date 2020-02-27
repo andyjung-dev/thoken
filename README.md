c3c21ad288c006f687ac80ffe7e235919a76e647
andyjung-dev <andy_jng@outlook.com>
2020-02-26 21:54:27 -0500

    some stuff

---
+++ README.md
@@ -0,0 +1,4 @@
+# Thoken (a quick and dirty analyzer)
+
+Currently the value is in the data, not in any code.
+Only pulls crypto data from a sqlite db, exposes it as JSON in a REST API, then renderd in `charts.js` made possible with `Vue`
---
+++ frontends/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Linessss</h2>
+    <line-chart></line-chart>
+    
+  </div>
+</template>
+
+<script>
+
+// import axios from 'axios';
+console.log("test");
+
+import LineChart from '@/components/LineChart';
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    LineChart
+  },
+  props: {
+    msg: String
+  },
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
